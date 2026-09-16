c38c19e63eaad576996d1b8ac396e6ec6dc02f59
Morquis <morcas@Morquis.>
2026-09-16 21:19:48 +0200

    CPP03 ex00 terminado

---
+++ cpp03/ex00/inc/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAPTRAP_HPP
+	#define CLAPTRAP_HPP
+
+	#include <iostream>
+	#include <string>
+
+	class ClapTrap {
+
+		public:
+			ClapTrap(void);
+			ClapTrap(const std::string &name);
+			ClapTrap(const ClapTrap &a);
+			ClapTrap& operator=(const ClapTrap &a);
+			~ClapTrap(void);
+			void attack(const std::string &target);
+			void takeDamage(unsigned int amount);
+			void beRepaired(unsigned int amount);
+		private:
+			std::string name;
+			unsigned int hp;
+			unsigned int energy;
+			unsigned int attackDamage;
+
+		};
+
+#endif
---
+++ cpp03/ex00/src/main.cpp
@@ -0,0 +1,24 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap clap1;              // Default constructor
+    ClapTrap clap2("CL4P-TP");   // Constructor con nombre
+
+    clap1.attack("Target1");     // Ataque con default
+    clap2.attack("Target2");
+
+    clap1.takeDamage(5);         // Tomar daño
+    clap2.takeDamage(12);
+
+    clap1.beRepaired(3);         // Reparación
+    clap2.beRepaired(6);
+
+    ClapTrap clap3(clap2);      // Copy constructor
+    clap3.attack("Target3");
+
+    clap1 = clap3;               // Assignment operator
+    clap1.attack("Target4");
+
+    return 0;
+}
